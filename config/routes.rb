2456496f36145b8c038a1fc51cb3ef7d3a8a8e2f
nujonakat1 <takanojun@icloud.com>
2023-02-09 19:07:18 +0900

    ルートURLの表示を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'worships/thank'
-  root "application#hello"
+  # get 'worships/thank'
+  # root "application#hello"
+  # HTTP名 "URL", to:"コントローラ名#コントローラのアクション名"
+  root to: "worships#thank"
+
+  resources :worships
 end
